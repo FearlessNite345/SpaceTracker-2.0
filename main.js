50bd6c08bcb21dd49c1f85fa10f374f4bf837f11
FearlessNite345 <Joshualambert345@gmail.com>
2023-04-25 20:02:33 -0400

    Small Patch

--- main.js
+++ main.js
@@ -1,22 +1,35 @@
-const { app, BrowserWindow } = require('electron')
+// Import the required modules
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
 
-const createWindow = () => {
-    const win = new BrowserWindow({
-      width: 800,
-      height: 600
-    })
-  
-    win.loadFile('home.html')
+// Create a new window and load the index.html file
+function createWindow() {
+  const mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js')
+    }
+  });
+
+  mainWindow.loadFile('home.html');
 }
 
+// Initialize the app and create the window when the app is ready
 app.whenReady().then(() => {
-    createWindow()
+  createWindow();
 
-    /* app.on('activate', () => {
-        if (BrowserWindow.getAllWindows().length === 0) createWindow()
-    }) */
-})
+  // Quit the app when all windows are closed
+  app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+      app.quit();
+    }
+  });
+});
 
-/* app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin') app.quit()
-}) */
+// Activate the app when no windows are open
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
